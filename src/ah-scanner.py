2f9f7b1402ac3fad688aa4a030404a63bd660cb6
Cayphas Antoine <a.koos.cayphas@gmail.com>
2015-11-06 21:51:43 +0100

    Git ignore
    Add git Ignore

--- src/ah-scanner.py
+++ src/ah-scanner.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import datetime
+import time
 
 class Scanner(object):
 	def __init__(self, realm, locale, api_key, lastModified=0):
@@ -15,7 +16,6 @@ class Scanner(object):
 		json_res = json.loads(s)
 		datas = urllib.request.urlopen(json_res["files"][0]["url"])
 		modif = json_res["files"][0]["lastModified"]
-		print(modif)
 		if modif != self.lastModified:
 			f = open("results/"+str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))+".json","w")
 			f.write(datas.read().decode(encoding='UTF-8'))
@@ -23,9 +23,14 @@ class Scanner(object):
 			open("config/AH-scanner.txt","w").write(str(modif))
 
 def main():
-	lastModified = int(open("config/AH-scanner.txt").read().strip())
-	scan = Scanner("garona","fr_FR","pjwwk5ex8gwtekdfyftes7wwy5bcuzmh", lastModified)
-	scan.scan()
+	while True:
+		try:
+			lastModified = int(open("config/AH-scanner.txt").read().strip())
+			scan = Scanner("garona","fr_FR","pjwwk5ex8gwtekdfyftes7wwy5bcuzmh", lastModified)
+			scan.scan()
+		except Exception as e:
+			print(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))+"error")
+		time.sleep(60)
 
 if __name__ == '__main__':
 	main()
